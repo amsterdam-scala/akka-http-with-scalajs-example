478f6010b2ac76f42b57f8bc38e4e83a8a2a4e1f
SeriousSoftware <vdenberg+github@gmail.com>
2017-02-14 11:43:10 +0100

    feat: WiP

--- WebApp/client/src/main/scala/example/BootstrapTestApp.scala
+++ WebApp/client/src/main/scala/example/BootstrapTestApp.scala
@@ -10,40 +10,43 @@ import rx._
 import scala.language.postfixOps
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
-import scalatags.JsDom.all.{img, _}
+import scalatags.JsDom.all._
 
 @JSExport
 object BootstrapTestApp extends JSApp {
   private implicit val context = implicitly[Ctx.Owner] // Stops working if moved to main(), macro magic
 
   def main(): Unit = {
-    //jQuery(() ⇒ {
 
-    def navigationBar = NavigationBar()
-      .withBrand(div(a("Akka HTTP RESTless web app", href := "http://getbootstrap.com/components/#navbar"),
-        a(href := "http://www.scala-lang.org/", target := "_blank")
-        (img(src := "/img/scala-5.svg", height := 42, width:=42, title := "Scala language")),
+    def navigationBar = {
+      val imgStyle = style := "height : 42px; width : 42px"
 
-        a(href := "http://www.scala-js.org/", target := "_blank")
-        (img(src := "/img/scala-js-logo.svg", height := 42, title := "Scala.js")),
+      NavigationBar2()
+        .withBrand(div(a(href := "http://getbootstrap.com/components/#navbar")("AkkaHTTP", br, "RESTless"),
+          a(href := "http://www.scala-lang.org/", target := "_blank")
+          (img(imgStyle)(src := "/img/scala-5.svg", title := "Scala language")),
 
-        a(href := "http://doc.akka.io/docs/akka-http/10.0.1/scala/http/introduction.html", target := "_blank")
-        (img(src := "/img/akka512.png", height := 42, width := 42, title := "The Streaming-first HTTP server/module of Akka.")),
+          a(href := "http://www.scala-js.org/", target := "_blank")
+          (img(imgStyle)(src := "/img/scala-js-logo.svg", title := "Scala.js")),
 
-        //img(src := "/img/logo-React.svg", height := "42", attr("height") := "42", title := "Facebook's React"),
-        img(src := "/img/api-graphic.jpg", height := 42, title := "RESTless web service"),
+          a(href := "http://doc.akka.io/docs/akka-http/10.0.1/scala/http/introduction.html", target := "_blank")
+          (img(imgStyle)(src := "/img/akka512.png", title := "The Streaming-first HTTP server/module of Akka.")),
 
-        img(src := "/img/logo-Bootstrap.svg", height := 42, title := "Twitter Bootstrap"))
+          //img(src := "/img/logo-React.svg", height := "42", attr("height") := "42", title := "Facebook's React"),
+          img(imgStyle)(src := "/img/api-graphic.jpg", title := "RESTless web service"),
 
-      )
-      .withTabs(
-        //          NavigationTab(tabTitle, "table", "table".fontAwesome(FontAwesome.fixedWidth), new TestTable, tableVisible.reactiveShow),
-        NavigationTab("Carousel", "carousel", "picture", new TodoList()),
-        NavigationTab("ToDo list", "todo", "fort-awesome".fontAwesome(FontAwesome.fixedWidth), new TodoList())
-      )
-      .withContentContainer(content ⇒ GridSystem.container(id := "main-container", GridSystem.mkRow(content)))
-      .withStyles(NavigationBarStyle.inverse, NavigationBarStyle.fixedTop, NavigationBarStyle.default)
-      .build()
+          img(imgStyle)(src := "/img/logo-Bootstrap.svg", title := "Twitter Bootstrap"))
+
+        )
+        .withTabs(
+          //          NavigationTab(tabTitle, "table", "table".fontAwesome(FontAwesome.fixedWidth), new TestTable, tableVisible.reactiveShow),
+          NavigationTab("Carousel", "carousel", "picture", new TodoList()),
+          NavigationTab("ToDo list", "todo", "fort-awesome".fontAwesome(FontAwesome.fixedWidth), new TodoList())
+        )
+        .withContentContainer(content ⇒ GridSystem.container(id := "main-container", GridSystem.mkRow(content)))
+        .withStyles(NavigationBarStyle.inverse, NavigationBarStyle.fixedTop)
+        .build()
+    }
 
     // Render page
     navigationBar.applyTo(dom.document.body)
@@ -51,6 +54,7 @@ object BootstrapTestApp extends JSApp {
     // Reactive navbar test
     //      navigationBar.addTabs(NavigationTab("Buttons", "buttons", "log-in", new TestPanel("Serious business panel", PanelStyle.warning)))
     //navigationBar.selectTab(0)
-    /*})*/
   }
+
+
 }
